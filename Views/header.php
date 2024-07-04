9b9dc769d8cc41655baccc39d051438b8febe51a
AslanPiab <p-aslan@hotmail.fr>
2024-07-04 11:41:07 +0200

    ajout toggle switch pour ajouter un dark mode

--- Views/header.php
+++ Views/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -7,29 +8,36 @@
     <link href="css/style.css" rel="stylesheet">
     <title>Gesti Tache</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">
-        <img src="img/logoTacheTic.png" alt="Logo TâcheTic">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            <?php if (isset($_SESSION['user_id'])): ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?action=logout">Se déconnecter</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">
+            <img src="img/logoTacheTic.png" alt="Logo TâcheTic">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item switch-container">
+                    <label class="switch">
+                        <input type="checkbox">
+                        <span class="slider round"></span>
+                    </label>
                 </li>
-            <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?action=login">Se connecter</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?action=register">S'inscrire</a>
-                </li>
-            <?php endif; ?>
-        </ul>
-    </div>
-</nav>
-<div class="container mt-4">
+                <?php if (isset($_SESSION['user_id'])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=logout">Se déconnecter</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=login">Se connecter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=register">S'inscrire</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-4">
