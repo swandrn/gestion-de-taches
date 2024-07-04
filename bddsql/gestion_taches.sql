03c78ff3877e0a388985227bc516d06a3259f11f
AslanPiab <p-aslan@hotmail.fr>
2024-07-04 09:29:20 +0200

    ajout nouvelle bddsql gestion_taches

--- bddsql/gestion_taches.sql
+++ bddsql/gestion_taches.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 02 juil. 2024 à 07:53
+-- Généré le : jeu. 04 juil. 2024 à 09:27
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -33,8 +33,20 @@ CREATE TABLE `taches` (
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   `priorite` int(11) DEFAULT NULL,
-  `date_echeance` date DEFAULT NULL
+  `date_echeance` date DEFAULT NULL,
+  `is_completed` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `taches`
+--
+
+INSERT INTO `taches` (`id`, `utilisateur_id`, `title`, `description`, `priorite`, `date_echeance`, `is_completed`) VALUES
+(2, 1, 'hasta luego go', 'bande organisé', 1, '2024-07-19', 0),
+(6, 2, 'Faire le ménage', 'Passer le balais, La serpillère', 2, '2024-07-18', 1),
+(8, 2, 'Aller à la boite aux lettres', 'Récupérer le courrier', 1, '2025-02-20', 0),
+(9, 2, 'Trouver un stage', 'Relancer les entreprises', 0, '2024-07-11', 0),
+(10, 2, 'Faire les courses', 'Acheter du Nutella', 0, '2025-07-10', 0);
 
 -- --------------------------------------------------------
 
@@ -48,6 +60,14 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `mot_de_passe` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `nom`, `email`, `mot_de_passe`) VALUES
+(1, 'Piab', 'test@test.fr', '$2y$10$85oHvR6y.ZG3ih/QEZRQguYkZGFpayCXxQoKS5/2lYoO8u9Niv/CC'),
+(2, 'Pierre', 'tt@tt.fr', '$2y$10$NFhln8w2B29N/wMfOalRU.SNLdReZXIJwiaQcSp6I2xR6jBkXI3sa');
 
 --
 -- Index pour les tables déchargées
@@ -75,13 +95,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `taches`
 --
 ALTER TABLE `taches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
